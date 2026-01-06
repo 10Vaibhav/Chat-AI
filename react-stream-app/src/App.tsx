0ac97b8cd9bb67c2f302f3667f60b6449833a566
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2026-01-06 23:48:23 +0530

    add Login component

--- react-stream-app/src/App.tsx
+++ react-stream-app/src/App.tsx
@@ -1,12 +1,35 @@
 import { ThemeProvider } from "./providers/theme-provider"
+import { Login } from "./components/login";
+import { type User } from "stream-chat";
+import { useState } from "react";
+
+const USER_STORAGE_KEY = "chat-ai-app-user";
 
 function App() {
 
+  const [user, setUser] = useState<User | null>(()=> {
+    const savedUser = localStorage.getItem(USER_STORAGE_KEY);
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  const handleUserLogin = (authenticatedUser: User) => {
+    const avatarUrl = `https://api.dicebear.com/9.x/avataaars/svg?seed=${authenticatedUser.name}`;
+    const userWithImage = {...authenticatedUser, image: avatarUrl,};
+
+    localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(userWithImage));
+
+    setUser(userWithImage);
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem(USER_STORAGE_KEY);
+    setUser(null);
+  }
 
   return (
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="h-screen bg-background">
-          Welcome to Stream Chat AI
+          <Login onLogin={handleUserLogin}/>
         </div>
       </ThemeProvider>
 
